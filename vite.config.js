e8afcd8f5916e27d65c42d4971a971ff00829be7
Nouar Chami <chaminouar37@gmail.com>
2025-07-15 02:18:11 +0200

    Fix: updated button color on homepage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+// import { defineConfig } from 'vite'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [reactRefresh()]
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [react()],
+});
+
